04506da7676aafeafc427d1d1650893274433d43
EtishaGarg <gargetisha@gmail.com>
2023-07-17 23:55:20 +0530

    Outline created

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: "./public/**/*.{html,js}",
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        'desktop': '1440px',
+      },
+    },
   },
   plugins: [],
 }
